975046a0041d4fb24b4eb0a07fcf30f8fb88caea
Fabian Braun <fabian.braun121@gmail.com>
2023-12-07 17:59:55 +0100

    ..

--- sensors.py
+++ sensors.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
 import RPi.GPIO as GPIO
-import time
+import uuid
+import threading
 GPIO.setmode(GPIO.BCM)
 
 SLEEP_TIME = 0.5
 
 class Sensor(ABC):
-    def __init__(self, uuid):
-        self.uuid = uuid
+    def __init__(self):
+        self.uuid = uuid.uuid4()
         self.state = 0
 
     @abstractmethod
@@ -15,19 +16,20 @@ class Sensor(ABC):
         pass
 
     def track_state_change(self):
-        while True:
-            if self.get_sensor_state() != self.state:
-                self.state =  self.get_sensor_state()
-                print(f'change in state to f{self.state}')
+        if self.get_sensor_state() != self.state:
+            self.state =  self.get_sensor_state()
+            print(f'change in state to f{self.state}')
+        threading.Timer(SLEEP_TIME, self.track_state_change).start()
+        
 
 class IRMovementSensor(Sensor):
-    def __init__(self, uuid, PIN):
-        super().__init__(uuid)
+    def __init__(self, PIN):
+        super().__init__()
         self.PIN = PIN
         GPIO.setup(self.PIN, GPIO.IN)
     
     def get_sensor_state(self):
         return GPIO.input(self.PIN)
 
-sensor = IRMovementSensor(13, 18)
+sensor = IRMovementSensor(18)
 sensor.track_state_change()
