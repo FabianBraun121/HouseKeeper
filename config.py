4d563c8874afa2448581b5cdf199c340a32a4131
Fabian Braun <fabian.braun121@gmail.com>
2023-12-11 20:27:28 +0100

    a

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import json
 
 DEFAULT_CONFIG = {
     "central_ip": "192.168.178.38",
-    "initialization_gate": 1001,
-    "sensor_gate": 1002,
-    "reactor_gate": 1003,
+    "initialization_gate": 5001,
+    "sensor_gate": 5002,
+    "reactor_gate": 5003,
 
     "sensor_sleep_time": 0.5,
     "alarm_num_images": 5,
