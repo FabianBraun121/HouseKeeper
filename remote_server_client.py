31ca450d0ab86958062716e9a357c68b5ddc6b6c
Fabian Braun <fabian.braun121@gmail.com>
2023-12-16 11:38:25 +0100

    lets hope

--- remote_server_client.py
+++ remote_server_client.py
@@ -7,27 +7,27 @@ from concurrent.futures import ThreadPoolExecutor
 import threading
 from datetime import datetime, timedelta, timezone
 import time
+import boto3.session
 
 
 class RemoteServerClient():
     def __init__(self, config):
         self.cfg = config
-        self.b2 = self.create_boto_resource()
         self.delete_old_files(self.cfg.get('delete_images_after_n_days'))
 
-    def create_boto_resource(self):
-        return boto3.resource(
-            service_name='s3',
-            endpoint_url=self.cfg.get('endpoint'),
-            aws_access_key_id=self.cfg.get('key_id'),
-            aws_secret_access_key=self.cfg.get('application_key'),
-            config=Config(signature_version='s3v4')
-        )
+    def create_bucket(self):
+        return self.create_resource().Bucket(self.cfg.get('bucket'))
 
+    def delete_all_files(self):
+        bucket = self.create_bucket()
+        for obj in bucket.objects.all():
+            obj.delete()
+        
     def delete_old_files(self, days_threshold):
+        bucket = self.create_bucket()
         threshold_date = datetime.now().replace(tzinfo=timezone.utc) - \
             timedelta(days=days_threshold)
-        for obj in self.b2.Bucket(self.cfg.get('bucket')).objects.all():
+        for obj in bucket.objects.all():
             last_modified = obj.last_modified.replace(tzinfo=timezone.utc)
             if last_modified < threshold_date:
                 object_key = obj.key
@@ -36,14 +36,14 @@ class RemoteServerClient():
 
     def download_file(self, key_name, local_fname):
         try:
-            self.b2.Bucket(self.cfg.get('bucket')).download_file(
-                key_name, local_fname)
+            bucket = self.create_bucket()
+            bucket.download_file(key_name, local_fname)
         except ClientError as ce:
             print('error', ce)
 
     def get_file(self, key_name):
         try:
-            response = self.b2.get_object(
+            response = self.create_resource().get_object(
                 Bucket=self.cfg.get('bucket'), Key=key_name)
             return response['Body'].read()
         except ClientError as ce:
@@ -51,7 +51,7 @@ class RemoteServerClient():
 
     def get_file_info_list(self):
         try:
-            response = self.b2.Bucket(self.cfg.get('bucket')).objects.all()
+            response = self.create_bucket().objects.all()
             file_list = []
             for obj in response:
                 info = (obj.key, obj.last_modified.timestamp())
@@ -60,21 +60,21 @@ class RemoteServerClient():
         except ClientError as ce:
             print('error', ce)
 
+    def create_resource(self):
+        return boto3.resource(
+            service_name='s3',
+            endpoint_url=self.cfg.get('endpoint'),
+            aws_access_key_id=self.cfg.get('key_id'),
+            aws_secret_access_key=self.cfg.get('application_key'),
+            config=Config(signature_version='s3v4')
+        )
+
     def upload_file(self, fname, b2fname=None):
         if b2fname is None:
             b2fname = fname
         try:
-            thread = threading.Thread(
-                target=self._upload_file, args=(fname, b2fname))
-            thread.start()
-            thread.join()
+            bucket = self.create_bucket()
+            with open(fname, 'rb') as file_content:
+                bucket.put_object(Key=b2fname, Body=file_content)
         except Exception as e:
             print('Error uploading file:', e)
-
-    def _upload_file(self, fname, b2fname):
-        b2 = self.create_boto_resource()
-        b2.Bucket(self.cfg.get('bucket')).upload_file(fname, b2fname)
-        print(f'{fname} done')
-        time.sleep(10)
-        print(f'{fname} waiting done')
-
