bf5ea62dddde6f0d95091c4e8e7ddec125aa0311
Fabian Braun <fabian.braun121@gmail.com>
2023-12-16 01:56:23 +0100

    a

--- remote_server_client.py
+++ remote_server_client.py
@@ -60,35 +60,21 @@ class RemoteServerClient():
         except ClientError as ce:
             print('error', ce)
 
-#    def upload_file(self, fname, b2fname=None):
-#        if b2fname is None:
-#            b2fname = fname
-#        try:
-#            thread = threading.Thread(
-#                target=self._upload_file, args=(fname, b2fname))
-#            thread.start()
-#            thread.join()
-#        except Exception as e:
-#            print('Error uploading file:', e)
-#
-#    def _upload_file(self, fname, b2fname):
-#        b2 = self.create_boto_resource()
-#        b2.Bucket(self.cfg.get('bucket')).upload_file(fname, b2fname)
-#
-#
     def upload_file(self, fname, b2fname=None):
         if b2fname is None:
             b2fname = fname
-
         try:
-            with ThreadPoolExecutor(max_workers=1) as executor:
-                future = executor.submit(self._upload_file, fname, b2fname)
-                # Wait for the future to complete (upload to finish)
-                future.result()
+            thread = threading.Thread(
+                target=self._upload_file, args=(fname, b2fname))
+            thread.start()
+            thread.join()
         except Exception as e:
             print('Error uploading file:', e)
 
     def _upload_file(self, fname, b2fname):
         b2 = self.create_boto_resource()
         b2.Bucket(self.cfg.get('bucket')).upload_file(fname, b2fname)
-        time.sleep(15)
+        print(f'{fname} done')
+        time.sleep(10)
+        print(f'{fname} waiting done')
+
