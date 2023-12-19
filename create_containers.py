4db200c36a79ba81e1c618cfca0c3924a3454a17
Fabian Braun <fabian.braun121@gmail.com>
2023-12-19 10:46:18 +0100

    a

--- create_containers.py
+++ create_containers.py
@@ -3,8 +3,8 @@ import subprocess
 def create_container(component, line, *args):
     # Generate Dockerfile content
     dockerfile_content = f'''
-FROM your_base_image
-CMD ["python", "{component.lower()}.py", {", ".join(args)}"]
+FROM fabianbraun121/housekeeper:latest
+CMD ["python", "base_container/run_{component.lower()}.py", {", ".join(args)}"]
 '''
 
     # Write Dockerfile to disk
@@ -12,9 +12,16 @@ CMD ["python", "{component.lower()}.py", {", ".join(args)}"]
     with open(dockerfile_path, "w") as dockerfile:
         dockerfile.write(dockerfile_content)
 
-    # Build and run the Docker container
-    subprocess.run(["docker", "build", "-t", f"{component.lower()}-image{line}", "-f", dockerfile_path, "."])
-    subprocess.run(["docker", "run", "-d", f"--name={component.lower()}-container", f"{component.lower()}-image{line}"])
+    # Build and run the Docker container with specific flags
+    subprocess.run([
+        "docker", "build", "-t", f"{component.lower()}-image{line}", "-f", dockerfile_path, "."
+    ])
+    subprocess.run([
+        "docker", "run", "-it", "--rm", "--network", "host",
+        "-v", "~/Documents/HouseKeeper/base_container/secret_config.json:/app/Documents/HouseKeeper/base_container/secret_config.json",
+        "--privileged", "--env", "UDEV=1",
+        f"--name={component.lower()}-container", f"{component.lower()}-image{line}"
+    ])
 
 def main():
     with open("components.txt", "r") as file:
